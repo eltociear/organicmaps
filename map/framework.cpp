caa858d39bce38a0756d1ab39b8602c5c50a47c6
S. Kozyr <s.trump@gmail.com>
2023-08-18 11:43:57 +0300

    [routing] In helicopter routing mode single tap continues route to the point
    Signed-off-by: S. Kozyr <s.trump@gmail.com>

--- map/framework.cpp
+++ map/framework.cpp
@@ -2044,6 +2044,25 @@ void Framework::OnTapEvent(place_page::BuildInfo const & buildInfo)
 {
   auto placePageInfo = BuildPlacePageInfo(buildInfo);
 
+  if (m_routingManager.IsRoutingActive() && m_routingManager.GetCurrentRouterType() == routing::RouterType::Helicopter) {
+    // Continue route to the point
+    RouteMarkData data;
+    data.m_title = placePageInfo ? placePageInfo->GetTitle() : std::string();
+    data.m_subTitle = std::string();
+    data.m_pointType = RouteMarkType::Finish;
+    data.m_intermediateIndex = m_routingManager.GetRoutePointsCount();
+    data.m_isMyPosition = false;
+    data.m_position = buildInfo.m_mercator;
+
+    m_routingManager.ContinueRouteToPoint(std::move(data));
+
+    // Refresh route
+    m_routingManager.RemoveRoute(false);
+    m_routingManager.BuildRoute();
+
+    return;
+  }
+
   if (placePageInfo)
   {
     auto const prevTrackId = m_currentPlacePageInfo ? m_currentPlacePageInfo->GetTrackId()
